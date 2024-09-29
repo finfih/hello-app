4b83f87200cb32c2ec0c069d33e60310358251fa
finfih <89676885+finfih@users.noreply.github.com>
2024-09-29 20:56:22 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+st.title("HAPPY ANNIVERSARY")
+st.write("Hi bb i didn't quite made this website myself obv but i did code and write stuff of this blank black"
+         "screen so uh i just wanted to say that thank you for being with me since we met i really appreciate it "
+         "and even after all the stuff we went through too. I love you dearly and wish i could be by your side whenever"
+         "i want, or whenever you need me. You mean alot to me, and i don't want to lose you at all. You're my world"
+         "my universe, my everything. I will always support you on everything you do, i will always try to solve "
+         "every single problem we have. Sorry if i can't, or don't know how to comfort you sometimes. No, you're not"
+         "that hard to handle. It's just me not knowing how to handle you. These past few months made me realized"
+         "how much of a bad partner i am, and im sorry for hurting you in certain times. My queen, baby, wife, girlfriend"
+         "please no i will love you til the day i die even if you stop loving me i do not care. You are the most understanding"
+         "person that i've ever met. even if we argue sometimes, u would always find an excuse to blame yourself >:("
+         "not so cool. But anyways there's no limit into typing this out really, it's funny bcs im using a website to"
+         "type this out in a coding application. I actually wanted to code a game for you but i just started learning"
+         "the basics and i can't code a game in like 4 days. SO hope u enjoyed reading thissss i wuv youuu :3. "
+         "By fin<3")
